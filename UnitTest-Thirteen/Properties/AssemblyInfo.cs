ceac7d3d3d527af3dea57c4b369a97a54bc36489
alberttduong <albertduong0@gmail.com>
2024-04-14 20:30:36 -0700

    Added unit tests for Deck initialization methods.

---
+++ UnitTest-Thirteen/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest-Thirteen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest-Thirteen")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0cbf6314-d1a5-4e6e-a9dc-feb3a486b064")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest-Thirteen/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Thirteen_Game;
+
+namespace UnitTest_Thirteen
+{
+    [TestClass]
+    public class UnitTest_Deck
+    {
+        Deck deck = new Deck();
+        Deck.StandardDeck stdDeck = new Deck.StandardDeck();
+
+        [TestMethod]
+        public void Test_SuitAndNumToOrderedIndex()
+        {
+            Assert.AreEqual(Deck.suitAndNumToOrderedIndex(0, 0), 0);
+            Assert.AreEqual(Deck.suitAndNumToOrderedIndex(0, 12), 12);
+            Assert.AreEqual(Deck.suitAndNumToOrderedIndex(1, 0), 13);
+            Assert.AreEqual(Deck.suitAndNumToOrderedIndex(3, 12), 51);
+        }
+
+        [TestMethod]
+        public void Test_DeckInitialization()
+        {
+            IEnumerator<(int, int)> enumerator = stdDeck.GetEnumerator();
+            for (int i = 0; i < 52; i++)
+            {
+                enumerator.MoveNext();
+                (int suit, int num) = enumerator.Current;
+                Assert.AreEqual(deck.cards[i].suit, suit);
+                Assert.AreEqual(deck.cards[i].number, num);
+            }
+        }
+    }
+}
