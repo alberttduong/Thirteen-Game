cb2f021fa65bdee11a1ed345e9d41eb7ef289aca
alberttduong <albertduong0@gmail.com>
2024-04-14 18:57:20 -0700

    Added Suits enum and classes for Card, Deck, and Player.

--- Thirteen Game/Program.cs
+++ Thirteen Game/Program.cs
@@ -6,10 +6,51 @@ using System.Threading.Tasks;
 
 namespace Thirteen_Game
 {
+    public enum Suits
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts
+    }
+
+    public class Card
+    {
+        public int number { get; }
+        public string suit { get; }
+
+        public Card(int number, string suit)
+        {
+            this.number = number;
+            this.suit = suit;
+        }
+    }
+
+    public class Deck
+    {
+        private Card[] _cards = new Card[52];
+
+        public Deck()
+        {
+            
+        }
+    }
+
+    public class Player
+    {
+        public int id { get; set; }
+        public Player(int id)
+        {
+            this.id = id;
+        }
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
