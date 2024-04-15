9c438855ac88e944645de42e0a766c6f1b8dafb5
alberttduong <albertduong0@gmail.com>
2024-04-15 00:17:51 -0700

    Decks can be shuffled and they can print their cards.

--- Thirteen Game/Deck.cs
+++ Thirteen Game/Deck.cs
@@ -39,6 +39,33 @@ namespace Thirteen_Game
             }
         }
 
+        public void shuffleDeck()
+        {
+            // Swaps 2 random cards in the deck
+            // numShuffles times in order to shuffle the deck.
+
+            const int numShuffles = 300;
+            var random = new Random();
+
+            for (int i = 0; i < numShuffles; ++i)
+            {
+                int first = random.Next(52);
+                int second = random.Next(52);
+
+                Card temp = cards[first];
+                cards[first] = cards[second];
+                cards[second] = temp;
+            }
+        }
+
+        public void printDeck()
+        {
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card);   
+            }
+        }
+
         public Deck()
         {
             initializeCards();
